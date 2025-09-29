a8ccf93763a3a93371f3b13f69eb84ce2eb3742a
23rd <23rd@vivaldi.net>
2025-09-29 13:12:39 +0300

    Added ability to choose display for notifications to settings.

--- Telegram/SourceFiles/window/notifications_manager.h
+++ Telegram/SourceFiles/window/notifications_manager.h
@@ -42,6 +42,7 @@ class Track;
 } // namespace Media::Audio
 
 namespace Window {
+class Controller;
 class SessionController;
 } // namespace Window
 
@@ -465,5 +466,7 @@ protected:
 };
 
 [[nodiscard]] QString WrapFromScheduled(const QString &text);
+
+[[nodiscard]] QRect NotificationDisplayRect(Window::Controller *controller);
 
 } // namespace Window::Notifications
